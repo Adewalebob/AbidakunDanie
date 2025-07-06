02b71dab401b2dd2e60773dbae3d403f5cc047af
Adewalebob <ajayidaniel248@gmail.com>
2025-07-06 22:17:21 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# my first code
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Abidas1993/pen/jEPjgPz](https://codepen.io/Abidas1993/pen/jEPjgPz).
+
---
+++ script.js
@@ -0,0 +1,8 @@
+<head>
+    <title>My first html page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- Your content here -->
+    <script src="my first code"></script>
+</body>
